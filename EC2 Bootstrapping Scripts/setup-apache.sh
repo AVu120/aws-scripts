336a6a46b4d84c384c5a70284f164dcad26b399e
Anthony Hien Vu <anthony.vu@team.telstra.com>
2021-02-17 09:05:55 +1100

    Slight change to comment.

--- EC2 Bootstrapping Scripts/setup-apache.sh
+++ EC2 Bootstrapping Scripts/setup-apache.sh
@@ -2,7 +2,7 @@
 # Purpose: install & run httpd (Apache Web Server - Linux 2 version)
 # How: Copy and paste the contents of this file into the 'Step 3: Configure Instance Details > Advanced Details > User Data > As text' section when launching a new EC2 instance.
 # This script will immediately execute when the EC2 instance starts running. 
-# EC2 User Data is automatically run with the sudo command.
+# Each line will be automatically run with the sudo command.
 yum update -y
 yum install -y httpd.x86_64
 systemctl start httpd.service
