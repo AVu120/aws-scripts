d47bc6534fbfbb8b7a3bf22c4024e17f56f5a2b5
Anthony Hien Vu <anthony.vu@team.telstra.com>
2021-02-17 08:53:56 +1100

    Rename files and add instructional comments to setup-apache.sh.

---
+++ EC2 Bootstrapping Scripts/setup-apache.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# Purpose: install & run httpd (Apache Web Server - Linux 2 version)
+# How: Copy and paste the contents of this file into the 'Step 3: Configure Instance Details > Advanced Details > User Data > As text' section when launching a new EC2 instance.
+# This script will immediately execute when the EC2 instance starts running. 
+# EC2 User Data is automatically run with the sudo command.
+yum update -y
+yum install -y httpd.x86_64
+systemctl start httpd.service
+systemctl enable httpd.service
+echo "Hello World from $(hostname -f)" > /var/www/html/index.html
